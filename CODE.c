8ffd3f58bc993727f5e85fd19807f1ab08c62e47
Fatih Mar <mfatihmar@gmail.com>
2023-05-06 02:40:20 +0100

    minor update

--- CODE.c
+++ CODE.c
@@ -4,7 +4,7 @@ int main()
 {
     printf("[Name] Game Networking Resources\n");
     printf("[Desc] A Curated List of Game Network Programming Resources\n");
-    printf("[Repo] https://github.com/ThusWroteNomad/GameNetworkingResources.git\n");
+    printf("[Repo] https://github.com/ThusSpokeNomad/GameNetworkingResources.git\n");
 
     return 0;
 }
